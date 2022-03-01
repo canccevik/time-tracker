5d38257f5d96bdc122f1464848824f3ad3b12bd3
Can Çevik <mazlumcancevik@gmail.com>
2022-03-02 00:28:38 +0300

    Add time record model adapter

---
+++ lib/domain/models/time_record/time_record.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'time_record.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TimeRecordModelAdapter extends TypeAdapter<TimeRecordModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TimeRecordModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TimeRecordModel(
+      date: fields[0] as DateTime,
+      startDate: fields[1] as DateTime,
+      stopDate: fields[2] as DateTime,
+      totalTime: fields[3] as int,
+      isItOffDay: fields[4] == null ? false : fields[4] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TimeRecordModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.date)
+      ..writeByte(1)
+      ..write(obj.startDate)
+      ..writeByte(2)
+      ..write(obj.stopDate)
+      ..writeByte(3)
+      ..write(obj.totalTime)
+      ..writeByte(4)
+      ..write(obj.isItOffDay);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TimeRecordModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
